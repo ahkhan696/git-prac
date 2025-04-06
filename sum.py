cad32542451b97fea2f4e7b8d6a064731363f108
M. Salman Farsi <just.farsie@gmail.com>
2025-04-06 13:11:08 +0500

    take two numbers from user and add

--- sum.py
+++ sum.py
@@ -1,3 +1,4 @@
-#sum of two fixed numbers in python:
-a, b = 9, 11
-print(f"sum of 9 and 11 = {a + b}")
+#sum of two numbers in python:
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print(f"sum of these two numbers = {a + b}")
