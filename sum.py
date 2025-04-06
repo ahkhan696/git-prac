963289edd9b5781a9c86bfec858c14e677e46d14
ahkhan696 <hafizabdulhanan@gmail.com>
2025-04-06 13:18:16 +0500

    code is updated to using a function

--- sum.py
+++ sum.py
@@ -1,5 +1,10 @@
-#sum of two numbers in python:
+#sum of three numbers in python using function:
+def sum_three(a, b, c):
+    sum = a + b + c
+    return sum
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 c = int(input("Enter third number: "))
-print(f"sum of these three numbers = {a + b + c}")
+sum_result = sum_three(a, b, c)
+print(f"sum of these three numbers = {sum_result}")
+
