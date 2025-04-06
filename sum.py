8ee364b7f6360f6b6c780aa651105b0f4a1eb154
M. Salman Farsi <just.farsie@gmail.com>
2025-04-06 14:20:01 +0500

    re update the master file

--- sum.py
+++ sum.py
@@ -1,2 +1,10 @@
-a, b = 9, 10
-print(f"sum = {a + b}")
+#sum of three numbers in python using function:
+def sum_three(a, b, c):
+    sum = a + b + c
+    return sum
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+sum_result = sum_three(a, b, c)
+print(f"sum of these three numbers = {sum_result}")
+# farsi please check if it auto updated in sublime or not
