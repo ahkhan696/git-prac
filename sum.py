a66ad14fdb293ef72c1b072dcb545bc7ccc0853c
M. Salman Farsi <just.farsie@gmail.com>
2025-04-06 14:22:37 +0500

    remove the extra comment

--- sum.py
+++ sum.py
@@ -7,4 +7,3 @@ b = int(input("Enter second number: "))
 c = int(input("Enter third number: "))
 sum_result = sum_three(a, b, c)
 print(f"sum of these three numbers = {sum_result}")
-# farsi please check if it auto updated in sublime or not
